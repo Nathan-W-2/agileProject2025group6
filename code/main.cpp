d9fd647bf8783fb6de04fa602a26c514d6db5491
AlejandroT23 <atoledo1@carthage.edu>
2025-05-06 13:51:57 -0500

    Updated takeAttend()

--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 
 void menu();
 void takeAttend();
@@ -62,3 +64,5 @@ void takeAttend() {
         cout << "========================" << endl;
     }
 }
+
+
