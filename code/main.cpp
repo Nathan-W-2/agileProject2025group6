3d5595670b382efd59d5941b67d1dd94ed3247fe
AlejandroT23 <atoledo1@carthage.edu>
2025-05-06 13:14:42 -0500

    Started menu

--- code/main.cpp
+++ code/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout<<"Agile Project"<<endl;
+void menu();
+
+int main() {
+    
+    menu();
     return 0;
+}
+
+void menu() {
+    int selectionNum = 0;
+    
+    while (selectionNum != 3)
+    {
+        cout << "========================" << endl;
+        cout << "Press the key for corresponding action, or type out 3 to quit" << endl;
+        cout << "1) Take Attendance" << endl;
+        cout << "2) View Attendance" << endl;
+        cout << "========================" << endl;
+        
+        cin >> selectionNum;
+    }
 }
